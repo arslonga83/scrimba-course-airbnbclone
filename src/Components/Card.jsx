cac44436bb961c4dbe6914cfbad4a00af9279cfb
arslonga83 <jlwood83@gmail.com>
2022-10-11 16:07:57 -0700

    add sold out online badge

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,22 @@
 import star from '../assets/Star 1.png'
 
-function Card({img, rating, reviewCount, country, title, price}) {
-  
+function Card({img, rating, reviewCount, location, title, price, openSpots}) {
+  let badgeText
+  if (openSpots === 0) {
+    badgeText = 'SOLD OUT'
+  } else if (location === 'Online') {
+    badgeText = 'Online'
+  }
+
   return (
     <div className='card'>
+      {badgeText && <div className='card-badge'>{badgeText}</div>}
       <img src={`/src/assets/${img}`} className='pic'/>
       <div className='stats'>
         <img src={star} />
         <span>{rating}</span>
         <span className='grey'>({reviewCount}) • </span>
-        <span className='grey'>{country}</span>
+        <span className='grey'>{location}</span>
       </div>
       <p>{title}
 </p>
